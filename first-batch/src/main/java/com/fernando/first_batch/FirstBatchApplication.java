fe68a3fcf098051d9753d3d1be21f56e144b950d
f.areias <nando.calheirosx@gmail.com>
2024-09-22 20:10:10 -0300

    Init batch

---
+++ first-batch/src/main/java/com/fernando/first_batch/FirstBatchApplication.java
@@ -0,0 +1,13 @@
+package com.fernando.first_batch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstBatchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstBatchApplication.class, args);
+	}
+
+}
---
+++ first-batch/src/test/java/com/fernando/first_batch/FirstBatchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fernando.first_batch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstBatchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
